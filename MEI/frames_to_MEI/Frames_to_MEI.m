0e59703c032aa7b402d21323649d8d88721ec7c1
yemenoglu <132007585+yemenoglu@users.noreply.github.com>
2025-03-28 03:35:39 +0300

    Frames_to_MEI.m

--- MEI/frames_to_MEI/Frames_to_MEI.m
+++ MEI/frames_to_MEI/Frames_to_MEI.m
@@ -1,14 +1,14 @@
-img_fld = "All_frames_1";
-img_url = dir(fullfile(img_fld ,"*.png"));
-          clr = [240 10 10];
+img_fld = "All_frames_1"; # image folder
+img_url = dir(fullfile(img_fld ,"*.png")); # We get the frames as "png".
+          clr = [240 10 10]; # Which color do you want to track? Write its RGB color code. We write the RGB color code of the red color.
 I_sil = zeros(270,480);
 cnt = 1;
 num = length(img_url);
-    for   i = 40:num-20
+    for   i = 40:num-20 # How many frames do you want to use for merging and tracking, set it from here.
     I = imread(fullfile(img_url(i).folder , img_url(i).name));
-    Ix = imresize(I , [270,480 ]);
-    [L,centers] = imsegkmeans(Ix,15);
-    dist = sum(( double(centers) - clr).^2,2);
+    Ix = imresize(I , [270,480 ]); # Make it resize 
+    [L,centers] = imsegkmeans(Ix,15); # Get binary image using L mask.
+    dist = sum(( double(centers) - clr).^2,2); # Take all as binary.
     [~,min_ind] = min(dist);
     I_sil = I_sil + double(L == min_ind);
         if mod(i,10) == 0
